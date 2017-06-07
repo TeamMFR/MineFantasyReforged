22a521c70f5e5ce4563fd96af337a920f199951c
Sirse <sirse.box@gmail.com>
2017-06-07 16:01:22 +0700

    Mark it as 2.8.13 unofficial

--- src/main/java/minefantasy/mf2/MineFantasyII.java
+++ src/main/java/minefantasy/mf2/MineFantasyII.java
@@ -59,7 +59,7 @@ import net.minecraftforge.common.config.Configuration;
 public class MineFantasyII {
 	public static final String MODID = "minefantasy2";
 	public static final String NAME = "MineFantasyII";
-	public static final String VERSION = "Alpha_2.8.12";
+	public static final String VERSION = "Alpha_2.8.13u";
 	public static final WorldGenMFBase worldGenManager = new WorldGenMFBase();
 
 	@SidedProxy(clientSide = "minefantasy.mf2.network.ClientProxyMF", serverSide = "minefantasy.mf2.network.CommonProxyMF")
