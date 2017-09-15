cc88013139729513cd7a4934b873f8f241fd0fb6
Sirse <sirse.box@gmail.com>
2017-09-15 23:42:28 +0700

    Added ASM patch for horse armor support
    Based on SanAndreasP code.

---
+++ src/main/java/minefantasy/mf2/asm/MFLoadingPlugin.java
@@ -0,0 +1,53 @@
+package minefantasy.mf2.asm;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import minefantasy.mf2.asm.transformers.EntityHorseTransformer;
+import net.minecraft.launchwrapper.IClassTransformer;
+
+import java.util.Map;
+
+@IFMLLoadingPlugin.MCVersion("1.7.10")
+public class MFLoadingPlugin implements IFMLLoadingPlugin, IClassTransformer {
+
+    @Override
+    public String[] getASMTransformerClass() {
+        ASMHelper.isASMEnabled = true;
+        /* TODO
+        Config option for ASM patches
+         */
+        return new String[]{this.getClass().getName()};
+    }
+
+    @Override
+    public String getModContainerClass() {
+
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+        ASMHelper.isSrg = (Boolean) data.get("runtimeDeobfuscationEnabled");
+        new Mappings();
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+
+        return null;
+    }
+
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] classBytes) {
+        if (transformedName.equals("net.minecraft.entity.passive.EntityHorse")) {
+            return EntityHorseTransformer.transform(name, transformedName, classBytes);
+        }
+
+        return classBytes;
+    }
+}
---
+++ src/main/java/minefantasy/mf2/item/armour/ItemHorseArmorMF.java
@@ -0,0 +1,8 @@
+package minefantasy.mf2.item.armour;
+
+import minefantasy.mf2.api.armour.ItemHorseArmorMFBase;
+
+
+public class ItemHorseArmorMF  {
+
+}
