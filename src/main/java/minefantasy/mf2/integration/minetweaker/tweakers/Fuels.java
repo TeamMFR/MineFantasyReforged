10e5c5e6b706ba7523801544fcb5b0ff26ce30a6
Sirse <sirse.box@gmail.com>
2017-11-02 02:00:44 +0700

    Travis CI deployment

---
+++ src/main/java/minefantasy/mf2/integration/minetweaker/tweakers/Fuels.java
@@ -0,0 +1,20 @@
+package minefantasy.mf2.integration.minetweaker.tweakers;
+
+import minetweaker.api.item.IItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+@ZenClass("mods.minefantasy.Fuel")
+public class Fuels {
+
+    @ZenMethod
+    public static void addCarbon(IItemStack stack, int uses) {
+
+    }
+
+    public static class addFuelAction {
+
+    }
+
+}
