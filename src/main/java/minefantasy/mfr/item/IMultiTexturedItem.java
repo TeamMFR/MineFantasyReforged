00885a8b23161cf7e337b3ad118b7b8b21d50080
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2020-05-22 11:11:36 +0200

    Removed unnecessary interface

--- src/main/java/minefantasy/mfr/item/IMultiTexturedItem.java
+++
@@ -1,8 +0,0 @@
-package minefantasy.mfr.item;
-
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-
-public interface IMultiTexturedItem {
-	ResourceLocation getModelName(ItemStack stack);
-}
