56f595c379603a6998f1ce6d5eaf122ea4306200
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2020-09-16 10:30:05 +0200

    fixed build fail with wrong nullable import

--- src/main/java/minefantasy/mfr/util/Utils.java
+++ src/main/java/minefantasy/mfr/util/Utils.java
@@ -1,6 +1,5 @@
 package minefantasy.mfr.util;
 
-import com.sun.istack.internal.Nullable;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemStack;
@@ -11,56 +10,58 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
 
+import javax.annotation.Nullable;
+
 public class Utils {
 
-    public static <T> T nullValue() {
-        return null;
-    }
+	public static <T> T nullValue() {
+		return null;
+	}
 
-    public static boolean doesMatch(ItemStack item1, ItemStack item2) {
-        return item2.getItem() == item1.getItem() && (item2.getItemDamage() == OreDictionary.WILDCARD_VALUE
-                || item2.getItemDamage() == item1.getItemDamage());
-    }
+	public static boolean doesMatch(ItemStack item1, ItemStack item2) {
+		return item2.getItem() == item1.getItem() && (item2.getItemDamage() == OreDictionary.WILDCARD_VALUE
+				|| item2.getItemDamage() == item1.getItemDamage());
+	}
 
-    public static TileEntityChest getOtherDoubleChest(TileEntity inv) {
-        if (inv instanceof TileEntityChest) {
-            TileEntityChest chest = (TileEntityChest) inv;
+	public static TileEntityChest getOtherDoubleChest(TileEntity inv) {
+		if (inv instanceof TileEntityChest) {
+			TileEntityChest chest = (TileEntityChest) inv;
 
-            TileEntityChest adjacent = null;
+			TileEntityChest adjacent = null;
 
-            if (chest.adjacentChestXNeg != null) {
-                adjacent = chest.adjacentChestXNeg;
-            }
+			if (chest.adjacentChestXNeg != null) {
+				adjacent = chest.adjacentChestXNeg;
+			}
 
-            if (chest.adjacentChestXPos != null) {
-                adjacent = chest.adjacentChestXPos;
-            }
+			if (chest.adjacentChestXPos != null) {
+				adjacent = chest.adjacentChestXPos;
+			}
 
-            if (chest.adjacentChestZNeg != null) {
-                adjacent = chest.adjacentChestZNeg;
-            }
+			if (chest.adjacentChestZNeg != null) {
+				adjacent = chest.adjacentChestZNeg;
+			}
 
-            if (chest.adjacentChestZPos != null) {
-                adjacent = chest.adjacentChestZPos;
-            }
+			if (chest.adjacentChestZPos != null) {
+				adjacent = chest.adjacentChestZPos;
+			}
 
-            return adjacent;
-        }
-        return null;
-    }
+			return adjacent;
+		}
+		return null;
+	}
 
-    public interface IItemPropertyGetterFix extends IItemPropertyGetter {
-        float applyPropertyGetter(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn);
+	public interface IItemPropertyGetterFix extends IItemPropertyGetter {
+		float applyPropertyGetter(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn);
 
-        static IItemPropertyGetterFix create(final IItemPropertyGetterFix lambda) {
-            return lambda;
-        }
+		static IItemPropertyGetterFix create(final IItemPropertyGetterFix lambda) {
+			return lambda;
+		}
 
-        @Override
-        @SideOnly(Side.CLIENT)
-        default float apply(final ItemStack stack, @Nullable final World worldIn, @Nullable final EntityLivingBase entityIn) {
-            return applyPropertyGetter(stack, worldIn, entityIn);
-        }
-    }
+		@Override
+		@SideOnly(Side.CLIENT)
+		default float apply(final ItemStack stack, @Nullable final World worldIn, @Nullable final EntityLivingBase entityIn) {
+			return applyPropertyGetter(stack, worldIn, entityIn);
+		}
+	}
 
 }
