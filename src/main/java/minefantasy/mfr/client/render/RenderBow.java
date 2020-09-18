99ca96524dba7a709d3473747699b2dfe64e85a0
ThatPolishKid99 <sebichrzan99@gmail.com>
2020-09-17 21:36:05 -0500

    -Slightly adjusted arrow pullback

--- src/main/java/minefantasy/mfr/client/render/RenderBow.java
+++ src/main/java/minefantasy/mfr/client/render/RenderBow.java
@@ -33,58 +33,61 @@ public class RenderBow extends WrappedItemModel implements IItemRenderer {
 			pulling = entity.isHandActive() && entity.getActiveItemStack() == stack;
 		}
 
-		GlStateManager.pushMatrix();
+		GlStateManager.pushMatrix(); // bow start
 		for (int layer = 0; layer < 3; layer++) {
 
 			GlStateManager.pushMatrix();
-			int colour = CustomToolHelper.getColourFromItemStack(stack, layer);
+			int colour = CustomToolHelper.getColourFromItemStack(stack, layer); //get bow coloration
 			float red = (colour >> 16 & 255) / 255.0F;
 			float green = (colour >> 8 & 255) / 255.0F;
 			float blue = (colour & 255) / 255.0F;
 
-			GlStateManager.color(red, green, blue, 1.0F);
+			GlStateManager.color(red, green, blue, 1.0F); //set bow coloration
+
 			renderWrapped(stack);
-			GlStateManager.color(1F, 1F, 1F);
+
+			GlStateManager.color(1F, 1F, 1F); //reset color (?)
 			GlStateManager.popMatrix();
 		}
-		GlStateManager.popMatrix();
+		GlStateManager.popMatrix(); // bow end
 
-		if (!arrowStack.isEmpty() && AmmoMechanicsMFR.isFirearmLoaded(stack)) {
+		if (!arrowStack.isEmpty() && AmmoMechanicsMFR.isFirearmLoaded(stack) ) {
 			for (int layer = 0; layer < 3; layer++) {
 
-				GlStateManager.pushMatrix();
-
-				int colour = CustomToolHelper.getColourFromItemStack(arrowStack, layer);
-				float red = (colour >> 16 & 255) / 255.0F;
-				float green = (colour >> 8 & 255) / 255.0F;
-				float blue = (colour & 255) / 255.0F;
-
-				GlStateManager.color(red, green, blue, 1.0F);
+				GlStateManager.pushMatrix(); //arrow start
 
 				if (pulling) {
 
 					float pull = Math.min(!(entity.getActiveItemStack().getItem() instanceof ItemBow) ? 0.0F : (float) (stack.getMaxItemUseDuration() - entity.getItemInUseCount()) / 20.0F, 1.0F);
-
-					if (pull > 0 && pull < 0.3) {
+					if (pull > 0 && pull < 0.7F) {
 						pull = 0;
-					} else if (pull < 0.6) {
-						pull = 0.3F;
-					} else if (pull < 0.9) {
-						pull = 0.6F;
+					} else if (pull <  0.8F) {
+						pull = 0.4F;
+					} else if (pull <  0.9F) {
+						pull = 0.7F;
 					} else {
 						pull = 1F;
 					}
 
 					GlStateManager.translate((4F + (pull * 4)) / 16F, (12F - (pull * 4)) / 16F, 8.1F / 16F);
-				} else {
+				}
+				else {
 					GlStateManager.translate(4F / 16F, 12F / 16F, 8.1F / 16F);
 				}
 				GlStateManager.rotate(90, 0, 0, 1);
 
-				Minecraft.getMinecraft().getRenderItem().renderItem(arrowStack, TransformType.NONE);
-				GlStateManager.color(1F, 1F, 1F);
+				int colour = CustomToolHelper.getColourFromItemStack(arrowStack, layer); // get arrow coloration
+				float red = (colour >> 16 & 255) / 255.0F;
+				float green = (colour >> 8 & 255) / 255.0F;
+				float blue = (colour & 255) / 255.0F;
 
-				GlStateManager.popMatrix();
+				GlStateManager.color(red, green, blue, 1.0F); // set arrow coloration
+
+				Minecraft.getMinecraft().getRenderItem().renderItem(arrowStack, TransformType.NONE);
+
+				GlStateManager.color(1F, 1F, 1F); //reset coloration(?)
+
+				GlStateManager.popMatrix(); //arrow end
 			}
 		}
 	}
