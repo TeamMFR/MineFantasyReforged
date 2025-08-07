a0e1fef0dab76accc65799f81784c67c5a629fb9
ThatPolishKid99 <sebichrzan99@gmail.com>
2025-08-07 18:58:21 -0500

    fix: fixed Special and Tanner recipes from crashing due to TPK being dumb

--- src/main/java/minefantasy/mfr/registry/recipe/SpecialRecipe.java
+++ src/main/java/minefantasy/mfr/registry/recipe/SpecialRecipe.java
@@ -4,12 +4,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 
 public  class SpecialRecipe extends SpecialRecipeBase {
-	protected Ingredient input;
-	protected Ingredient specialInput;
-	protected ItemStack output;
-	protected String research;
-	protected String design;
-
 	public SpecialRecipe(ItemStack output, Ingredient input, Ingredient specialInput, String research, String design) {
 		super(output, input, specialInput, research, design);
 	}
